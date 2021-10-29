3423be2742c4c6c4457281c7b6fae7f6ec745557
Joey <jyudel@gmail.com>
2021-10-29 15:39:09 -0400

    Minor fix

--- pt_full_procedure.py
+++ pt_full_procedure.py
@@ -1,9 +1,10 @@
 import pt_predictor, save_model_as_spreadsheet
+from utils import DATA_DIR
 
 if __name__ == '__main__':
         version = "v2_exhaustive_iter_full_1029"
 
         pt_predictor.main(version=version)
 
-        save_model_as_spreadsheet.main(version=f'{DATA_DIR}/pt_{version}.pkl',
+        save_model_as_spreadsheet.spreadsheet_main(version=f'{DATA_DIR}/pt_{version}.pkl',
                                        filename=f'{DATA_DIR}/pt_predictions_{version}.xlsx')
