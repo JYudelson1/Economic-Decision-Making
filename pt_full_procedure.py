8b68cf6a83246f47f37e91a86a9850bdb6987bbc
Joey <jyudel@gmail.com>
2021-11-03 23:29:15 -0400

    Full procedures

--- pt_full_procedure.py
+++ pt_full_procedure.py
@@ -2,9 +2,10 @@ import pt_predictor, save_model_as_spreadsheet
 from utils import DATA_DIR
 
 if __name__ == '__main__':
-        version = "v2_exhaustive_iter_full_1029"
+        version = "exhaustive_iter_full_111"
 
         pt_predictor.main(version=version)
 
         save_model_as_spreadsheet.spreadsheet_main(version=f'{DATA_DIR}/pt_{version}.pkl',
-                                       filename=f'{DATA_DIR}/pt_predictions_{version}.xlsx')
+                                       filename=f'{DATA_DIR}/pt_predictions_{version}.xlsx',
+                                       use_all_fits=False)
---
+++ pt_tw_full_procedure.py
@@ -0,0 +1,11 @@
+import pt_tw_predictor, save_model_as_spreadsheet
+from utils import DATA_DIR
+
+if __name__ == '__main__':
+        version = "exhaustive_iter_full_111"
+
+        pt_tw_predictor.main(version=version)
+
+        save_model_as_spreadsheet.spreadsheet_main(version=f'{DATA_DIR}/pt_tw_{version}.pkl',
+                                       filename=f'{DATA_DIR}/pt_tw_predictions_{version}.xlsx',
+                                       use_all_fits=False)
